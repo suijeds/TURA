109f477647c4ee44f8c2dc03dd4745126bc10e11
suijeds <huseensajad972@gmail.com>
2026-06-20 13:18:35 +0300

    Trust deployment host inside NextAuth configuration

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import Google from "next-auth/providers/google"
 import { ConvexAdapter } from "./components/convex-adapter"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
